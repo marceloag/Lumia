2a834cb7272582f2ad259d1214ebdc344ffe7c97
Marcelo Aguila <web@croop.cl>
2014-12-08 14:13:41 -0300

    test

--- merge.php
+++ merge.php
@@ -5,7 +5,7 @@ $mask = new Imagick('mask.png');
 $over = new Imagick('over.png');
 
 // Setting same size for all images
-$base->resizeImage(274, 275, Imagick::FILTER_LANCZOS, 1);
+// $base->resizeImage(274, 275, Imagick::FILTER_LANCZOS, 1);
 
 // Copy opacity mask
 $base->compositeImage($mask, Imagick::COMPOSITE_DSTIN, 0, 0, Imagick::CHANNEL_ALPHA);
